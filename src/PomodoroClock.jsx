70961a33c4a90bff166c1ccfca430852c66b88fa
Farhan Abbas <fabbas.ambition@gmail.com>
2024-02-10 15:44:22 -0700

    made some changes to the css and jsx files

--- src/PomodoroClock.jsx
+++ src/PomodoroClock.jsx
@@ -1,60 +1,100 @@
-import { useState, useEffect } from 'react';
-import TimerDisplay from './TimerDisplay';
-import ControlButtons from './ControlButtons';
+import { useState, useEffect } from "react";
+import TimerDisplay from "./TimerDisplay";
+import ControlButtons from "./ControlButtons";
 
 export default function PomodoroClock() {
-    const [inputValue, setInputValue] = useState(NaN);
-    const [timerValue, setTimerValue] = useState(0);
-    const [start, setStart] = useState(false);
+	const [inputValue, setInputValue] = useState("");
+	const [timerValue, setTimerValue] = useState(0);
+	const [start, setStart] = useState(false);
 
-    const resetTimer = () => {
-        setInputValue(0);
-        setTimerValue(0);
-        setStart(false);
-    };
+	const resetTimer = () => {
+		setInputValue(0);
+		setTimerValue(0);
+		setStart(false);
+	};
 
     const startTimer = () => {
-        setTimerValue(inputValue*60);
+        if (timerValue === 0) {
+            setTimerValue(inputValue*60);
+        }
         setStart(true);
     };
 
-    const changeTimer = () => {
-        setTimerValue(inputValue*60);
-    };
+	const changeTimer = () => {
+		setTimerValue(inputValue * 60);
+	};
 
-    const pauseTimer = () => {
-        setStart(false);
-    };
+	const pauseTimer = () => {
+		setStart(false);
+	};
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
+	const handleChange = (event) => {
+		setInputValue(event.target.value);
+	};
 
-    useEffect(function myEffect(){
-        let intervalId;
-        if (start && timerValue > 0) {
-            intervalId = setInterval(() => {
-                setTimerValue(timerValue => timerValue - 1);
-            }, 1000);
-        }
+	useEffect(
+		function myEffect() {
+			let intervalId;
+			if (start && timerValue > 0) {
+				intervalId = setInterval(() => {
+					setTimerValue((timerValue) => timerValue - 1);
+				}, 1000);
+			}
 
-        return () => clearInterval(intervalId); // This clears the interval when the component unmounts
-    }, [start, timerValue]);
+			return () => clearInterval(intervalId); // This clears the interval when the component unmounts
+		},
+		[start, timerValue]
+	);
 
-    return (
-        <div style={{ padding:'0px 15px', backgroundColor: '#F1F2F0', opacity:'0.7' }}>
-            <h1 style = {{fontFamily:'Comic Sans MS', fontSize: '70px'}}>Pomodoro Clock</h1>
+	return (
+		<div
+			style={{
+				padding: "0px 15px",
+				backgroundColor: "#F1F2F0",
+				opacity: "0.7",
+			}}
+		>
+			<h1 style={{ fontFamily: "Comic Sans MS", fontSize: "70px" }}>
+				Pomodoro Clock
+			</h1>
 
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <h3 style = {{fontFamily:'Comic Sans MS', fontSize: '20px'}}>Enter time in minutes: </h3>
-                <input type = "number" value={inputValue} onChange={handleChange} placeholder = {0} style = {{padding:'10px 10px' , fontFamily:'Comic Sans MS', width: '50px', backgroundColor: 'transparent', border: '1px solid #000'}}></input>
-            </div>
-            
-            <div style = {{marginBottom: '20px'}}>
-                <button className="button" onClick={changeTimer}>Submit</button>
-            </div>
-            <TimerDisplay seconds={timerValue} />
-            <ControlButtons onStart={startTimer} onPause={pauseTimer} onReset={resetTimer} /> 
-        </div>
-    );
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<h3 style={{ fontFamily: "Comic Sans MS", fontSize: "20px" }}>
+					Enter time in minutes:{" "}
+				</h3>
+				<input
+					type="number"
+					value={inputValue}
+					onChange={handleChange}
+                    min={0}
+					placeholder={0}
+					style={{
+						padding: "10px 10px",
+						fontFamily: "Comic Sans MS",
+						width: "50px",
+						backgroundColor: "transparent",
+						border: "1px solid #000",
+					}}
+				></input>
+			</div>
+
+			<div style={{ marginBottom: "20px" }}>
+				<button className="button" onClick={changeTimer}>
+					Submit
+				</button>
+			</div>
+			<TimerDisplay seconds={timerValue} />
+			<ControlButtons
+				onStart={startTimer}
+				onPause={pauseTimer}
+				onReset={resetTimer}
+			/>
+		</div>
+	);
 }
--- src/TimerDisplay.jsx
+++ src/TimerDisplay.jsx
@@ -1,26 +1,43 @@
-    function secondsToTime(secs) {
-        let hours = Math.floor(secs / (60 * 60));
-    
-        let divisor_for_minutes = secs % (60 * 60);
-        let minutes = Math.floor(divisor_for_minutes / 60);
-    
-        let divisor_for_seconds = divisor_for_minutes % 60;
-        let seconds = Math.ceil(divisor_for_seconds);
-    
-        let obj = {
-            'h': hours.toString().padStart(2, '0'), // Add leading zero to hours
-            'm': minutes.toString().padStart(2, '0'), // Add leading zero to minutes
-            's': seconds.toString().padStart(2, '0') // Add leading zero to seconds
-        };
-        return obj;
-    }
+function secondsToTime(secs) {
+	let hours = Math.floor(secs / (60 * 60));
+
+	let divisor_for_minutes = secs % (60 * 60);
+	let minutes = Math.floor(divisor_for_minutes / 60);
+
+	let divisor_for_seconds = divisor_for_minutes % 60;
+	let seconds = Math.ceil(divisor_for_seconds);
+
+	let obj = {
+		h: hours.toString().padStart(2, "0"), // Add leading zero to hours
+		m: minutes.toString().padStart(2, "0"), // Add leading zero to minutes
+		s: seconds.toString().padStart(2, "0"), // Add leading zero to seconds
+	};
+	return obj;
+}
 
 export default function TimerDisplay({ seconds }) {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '10px', width: '350px', height: '100px', fontSize: '73px'}}>
-                {secondsToTime(seconds).h} : {secondsToTime(seconds).m} : {secondsToTime(seconds).s}
-            </div>
-        </div>
-    );
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					padding: "10px",
+					width: "350px",
+					height: "100px",
+					fontSize: "73px",
+				}}
+			>
+				{secondsToTime(seconds).h} : {secondsToTime(seconds).m} :{" "}
+				{secondsToTime(seconds).s}
+			</div>
+		</div>
+	);
 }
